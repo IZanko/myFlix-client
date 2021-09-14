95c9a79b4ae2b4127f13fb2204a245a748a695ea
IZanko <zankoiva@hotmail.com>
2021-09-14 11:34:05 -0400

    fetch data from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,36 +1,32 @@
 import React from "react";
+import axios from "axios";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import inceptionImg from "../../public/images/inception.jpg";
 import shawShankImg from "../../public/images/theshawshankredemption.jpg";
 import gladiatorImg from "../../public/images/gladiator.jpg";
 
+
 export class MainView extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'Inception',
-          Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
-          ImagePath: inceptionImg,
-        },
-        {
-          _id: 2,
-          Title: 'The Shawshank Redemption',
-          Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-          ImagePath: shawShankImg,
-        },
-        {
-          _id: 3,
-          Title: 'Gladiator',
-          Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
-          ImagePath: gladiatorImg,
-        }
-      ], selectedMovie: null
+      movies: [],
+      selectedMovie: null
     };
+  }
+
+  componentDidMount() {
+    axios.get("https://zanko-my-flix.herokuapp.com/movies")
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -40,7 +36,7 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view"></div>;
 
     return (
       <div className="main-view">
